267ce190b67c19960bfa5b1596abc5931187f2b2
amyyan210 <amy@yanaway.com>
2019-06-04 16:45:20 -0500

    Implement search functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -77,5 +77,16 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHeroes(term: string) {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching ${term}`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
   
 }
